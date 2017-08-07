d8a8275d42241a9be5e9af086d00fadb7dc1cd8d
Satya Mohapatra <mohapsat@gmail.com>
2017-08-07 00:49:40 -0700

    Added QRCODE_GEN
    Support endpoints:
    
    1) http://127.0.0.1:5000/tx/<text or url>
    returns:
    imgur link to qr code
    
    2) http://127.0.0.1:5000/raw/<text or url>
    returns:
    json response from imgur
    
    clientId has been redcated from privacy, please register on imgur api and get your own clientId

--- venv/utils/qrcode_gen.py
+++ venv/utils/qrcode_gen.py
@@ -50,7 +50,7 @@ def gen_raw_qrcode(inputText):
 
     album = None
     image_path = 'qrcode.png'
-    clientId = 'F5a0da0ee22b6b9'
+    clientId = 'xxxxxxxx'
 
     url = "https://api.imgur.com/3/image"
     payload = {'image': open(image_path, 'rb').read(), 'type': 'file'}
